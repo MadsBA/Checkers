412f6d0776f306159f7cc95a969b1f6599012930
madsBA <madsboeandersen03@gmail.com>
2024-03-06 14:29:31 +0100

    Forloop for board

--- Code/sketch.js
+++ Code/sketch.js
@@ -4,7 +4,8 @@ function setup() {
 
 function draw() {
   background(202);
-  for (let i; i > 7; i++){
-    rect(0,0,100,100);
+  for (let i=0; i < 7; i++){
+  rect(0+width/7*i,0,width/7,height/7);
+    console.log (i);
   }
 }
