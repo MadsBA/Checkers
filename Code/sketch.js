9de4ff4c6ac40e10eec8cadcff1f0e9b5aaf2717
madsBA <madsboeandersen03@gmail.com>
2024-03-06 15:07:15 +0100

    Board update
    need color

--- Code/sketch.js
+++ Code/sketch.js
@@ -5,7 +5,14 @@ function setup() {
 function draw() {
   background(202);
   for (let i=0; i < 7; i++){
-  rect(0+width/7*i,0,width/7,height/7);
-    console.log (i);
+    for (let j=0; j < 7; j++){
+      //fill(farve)
+      rect((0+(width-20)/7*i)+10,(0+(height-20)/7*j)+10,(width-20)/7,(height-20)/7);
+      //if (fill == 0){
+        //fill(220);
+      //} else {
+        //fill(0);
+      //}
+    }
   }
 }
