d25c606be568eb4aa24f2059861208e3fa790dc4
AlbinoDino <lucasbak@hotmail.dk>
2024-03-06 15:26:47 +0100

    Class File Made
    It might work

---
+++ Code/Brik.js
@@ -0,0 +1,18 @@
+
+class Brik{
+    constructor(x,y){
+        
+    BrikX = x;
+    BrikY = y;
+
+    }
+    update(){
+
+
+
+    }
+    show(){
+    circle(BirkX,BrikY,20)
+
+    }
+}
--- Code/sketch.js
+++ Code/sketch.js
@@ -1,5 +1,12 @@
+Brikker = [];
+MaxNr = 24;
+
 function setup() {
   createCanvas(840, 840);
+  for(let i = 0; i< MaxNr; i++){
+    Brikker.push(new Brik(random(0,width),random(0,height)));
+  }
+  
 }
 
 function draw() {
@@ -7,5 +14,6 @@ function draw() {
   for (let i=0; i < 7; i++){
   rect(0+width/7*i,0,width/7,height/7);
     console.log (i);
+    Brik.show();
   }
 }
