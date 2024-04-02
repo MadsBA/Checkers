0b8e1b50e007ceeda404387e199c27484a665356
AlbinoDino <lucasbak@hotmail.dk>
2024-04-02 14:54:56 +0200

    Its working

--- Code/Brik.js
+++ Code/Brik.js
@@ -1,15 +1,31 @@
-var BrikX = 0;
-var BrikY = 0;
+let BrikX = 0;
+let BrikY = 0;
+let Red = 0;
+let Green = 0;
+let Blue = 0;
 class Brik{
 
     constructor(x,y){   
     this.BrikX = x;
     this.BrikY = y;
-
+    if(this.BrikY < height/2){
+        this.Red = 255;
+        this.Green = 0;
+        this.Blue = 0;
+        } else {
+        this.Red = 0;
+        this.Green = 0;
+        this.Blue = 255;
+        }
     }
    
    Show(){
-   circle(this.BirkX,this.BrikY,20);
+   push()
+   fill(this.Red,this.Green,this.Blue)
+   circle(this.BrikX,this.BrikY,width/9);
+   pop()
    }
+   Update(){
 
+   }
 }
--- Code/sketch.js
+++ Code/sketch.js
@@ -1,10 +1,10 @@
-Brikker = [];
-MaxNr = 24;
+let Brikker = [];
+let MaxNr = 24;
 
 function setup() {
   createCanvas(840, 840);
   for(let i = 0; i< MaxNr; i++){
-    Brikker.push(new Brik(random(0,width),random(0,height)));
+    Brikker.push(new Brik(random(0,width),random(0,height),Math.round(0,1)));
   }
 }
 
@@ -13,9 +13,8 @@ function draw() {
   background(202);
   for (let i=0; i < 7; i++){
   rect(0+width/7*i,0,width/7,height/7);
-    console.log (i);
   }
-  Brik.Show();
-
-
+  for(let i = 0; i< MaxNr; i++){
+  Brikker[i].Show();
+  }
 }
