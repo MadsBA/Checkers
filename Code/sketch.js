8844be63f2d97cb85cf2acbf59bf31a2b1c9dab5
madsBA <madsboeandersen03@gmail.com>
2024-04-02 13:52:51 +0200

    Noter + slettet unødvendig kode

--- Code/sketch.js
+++ Code/sketch.js
@@ -4,14 +4,13 @@ function setup() {
 
 function draw() {
   background(100);
-  let col = 0;
-  let row = 0;
   let color1 = 0;
   let color2 = 255;
 
   for (let i=0; i < 8; i++){
     for (let j=0; j < 8; j++){
       
+      // % tjekker om det er et lige eller ulige tal for at automatisere farvelægningen af felter
       if (i%2 == 0){
         color1 = 255;
         color2 = 0;
@@ -19,13 +18,12 @@ function draw() {
         color1 = 0;
         color2 = 255;
       }
-
       if (j%2 == 0){
         fill (color1);
       } else {
         fill (color2);
       }
-
+      // Når det kommer til at lave selve firkanterne bruges variablerne i og j til at bestemme placeringen af firkanterne for at lave et fuldt bræt.
       rect((0+(width-20)/8*i)+10,(0+(height-20)/8*j)+10,(width-20)/8,(height-20)/8);
     }
   }
